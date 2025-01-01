209fbad34650d722f87fe0dbead63f7af57568e0
0x408 <morkvitnir@gmail.com>
2025-01-01 23:53:06 +0300

    feat: Add SCSS files for global styles and root variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
     modules: ["@nuxt/eslint"],
     devtools: { enabled: true },
+    css: ["~/assets/scss/main.scss", "~/assets/scss/root.scss"],
     compatibilityDate: "2024-11-01",
     eslint: {
         config: {
