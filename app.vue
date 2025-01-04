0fae2fecea3c9edf32b3900d9eac2d625b45a26a
0x408 <morkvitnir@gmail.com>
2025-01-04 16:27:46 +0300

    feat: Add favicon and web app manifest

--- app.vue
+++ app.vue
@@ -1,4 +1,11 @@
 <template>
+    <Head>
+        <Link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <Link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
+        <Link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <Link rel="manifest" href="/app.webmanifest" />
+    </Head>
+
     <div id="app">
         <NuxtRouteAnnouncer />
         <Logo />
