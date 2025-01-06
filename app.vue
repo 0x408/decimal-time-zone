28981eeffc5e7ff55d5ad755ca829c8590943dce
0x408 <morkvitnir@gmail.com>
2025-01-06 22:59:28 +0300

    feat: Implement SEO metadata

--- app.vue
+++ app.vue
@@ -1,3 +1,27 @@
+<script setup lang="ts">
+const config = useRuntimeConfig();
+
+useHead({
+    htmlAttrs: { lang: "en-US" },
+    title: "Decimal Time Zone | Modern Time Standard"
+});
+
+const description = ref("Time standard that combines the convenience of decimal time with the advantages of time zone systems.");
+
+useServerSeoMeta({
+    description: description,
+    keywords: "Decimal Time Zone, DTZ, ⸤DT⸣, decimal time, time zone, time standard",
+    ogTitle: "Modern Time Standard",
+    ogDescription: description,
+    ogImage: `${config.public.baseURL}/og-image.png`,
+    twitterCard: "summary_large_image",
+    ogUrl: config.public.baseURL,
+    ogType: "website",
+    ogSiteName: "Decimal Time Zone",
+    ogLocale: "en_US"
+});
+</script>
+
 <template>
     <Head>
         <Link rel="icon" href="/favicon.ico" sizes="48x48" />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
     modules: ["@nuxt/eslint"],
     devtools: { enabled: true },
     css: ["~/assets/scss/main.scss", "~/assets/scss/root.scss"],
+    runtimeConfig: {
+        public: {
+            baseURL: process.env.NUXT_PUBLIC_BASE_URL || "http://localhost:3000"
+        }
+    },
     compatibilityDate: "2024-11-01",
     eslint: {
         config: {
